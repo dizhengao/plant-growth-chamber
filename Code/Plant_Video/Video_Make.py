5098354b3c555f43b219b44dc48dd99eba7df37d
WeimingChe <cheweiming94@gmail.com>
2021-04-10 15:54:14 +0100

    a

---
+++ Code/Plant_Video/Video_Make.py
@@ -0,0 +1,18 @@
+p
+import cv2
+import numpy as np
+import glob
+ 
+img_array = []
+for filename in glob.glob('/Users/wc289/Google Drive (zd101iot@gmail.com)/210406/*.jpg'):# folder name
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width,height)
+    img_array.append(img)
+ 
+ 
+out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+ 
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
