230ea5ebe5e1fc1898c90724cf7ea95de6f4ac8c
WeimingChe <cheweiming94@gmail.com>
2021-07-20 20:47:44 +0100

    Update Arduino_Control_main_V2.ino

--- Components/Arduino_Control_main_V2/Arduino_Control_main_V2.ino
+++ Components/Arduino_Control_main_V2/Arduino_Control_main_V2.ino
@@ -6,6 +6,7 @@
 #include "RichUNOLM75.h" 
 #include "Sensors.h" 
 #include <multi_channel_relay.h>
+#include "RichUNODS1307.h"
 
 // bme680 sensor
 #define IIC_ADDR  uint8_t(0x76)
@@ -30,6 +31,10 @@ unsigned long StartTime;
 unsigned long CurrentTime;
 bool WaterRunning;
 
+// Time on Arduino Board
+DS1307 clock;//define a object of DS1307 class
+
+bool LightFanRun;
 
 void setup() {
   // put your setup code here, to run once:
@@ -46,6 +51,11 @@ void setup() {
 
    //Flag to show if water is running
     WaterRunning=0;
+
+    // Set up the clock
+    clock.begin();
+    // Flag to show if the light fun is operating
+    LightFanRun=1;
 }
 
 void loop() {
@@ -84,16 +94,18 @@ void loop() {
     Serial.print(bme680.sensor_result_value.humidity);
     Serial.print("\n");
 
-    // Temperature fan control
+    // Temperature fan control for the chamber
     // BIT(Relay_status,1) convert the status into binary number, the bit at 0 corresponds to switch 1.
     Relay_status=relay.getChannelState();
-    if (BIT(Relay_status,1)!=1 && Temp_LM75_built_in > 35){
-      relay.turn_on_channel(2);
+    if (BIT(Relay_status,3)!=1 && Temp_NTC_read > 25){
+      relay.turn_on_channel(4);
     }
 
-    if (BIT(Relay_status,1)==1 && Temp_LM75_built_in < 35){
-      relay.turn_off_channel(2);
+    if (BIT(Relay_status,1)==1 && Temp_NTC_read < 25){
+      relay.turn_off_channel(4);
     }
+
+    
 
     // Watering the system under command from PI
 
@@ -101,8 +113,8 @@ void loop() {
     // read the incoming byte:
     String Command = Serial.readStringUntil('\n');
     if (Command=="water"){
-      // turn on the relay switch 4
-      relay.turn_on_channel(4);
+      // turn on the relay switch 1
+      relay.turn_on_channel(1);
       // Mark the water running status as on
       WaterRunning = 1;
       // record the watering start time
@@ -111,12 +123,27 @@ void loop() {
   }
 
   // Watering time count down
-  if ( BIT(Relay_status,3)==1 && WaterRunning==1){
+  if ( BIT(Relay_status,0)==1 && WaterRunning==1){
     CurrentTime=millis();
     if(CurrentTime-StartTime>=10000){
-      relay.turn_off_channel(4);
+      relay.turn_off_channel(1);
       WaterRunning=0;
     }
   }
+   clock.getTime();
+   if (clock.hour>=0 && clock.hour<=8 && LightFanRun==1){
+        relay.turn_on_channel(2);
+        LightFanRun=0;
+   }
+   else if(LightFanRun==0 && clock.minute<=30){
+      relay.turn_off_channel(2);
+        LightFanRun=1;
+   }
+   else if(LightFanRun==1 && clock.minute>30){
+      relay.turn_on_channel(2);// turn off the light fan
+        LightFanRun=0;
+    }
+   
+   
     delay(1000);
 }
